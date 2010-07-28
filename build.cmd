ea9558490d81b7fdce82eaa517feadb35554d42b
Oliver, Jonathan <joliver@localhost>
2010-07-27 21:33:30 -0600

    Added ILMerge operations to bundle up different implementations.

--- build.cmd
+++ build.cmd
@@ -8,7 +8,16 @@ msbuild /nologo /verbosity:quiet src/StorageAccess.sln /p:Configuration=Release 
 msbuild /nologo /verbosity:quiet src/StorageAccess.sln /p:Configuration=Release
 
 echo Copying
-copy src\proj\StorageAccess.NHibernate\bin\Release\*.* output
+copy src\proj\StorageAccess\bin\Release\*.* output\
+
+echo Merging
+
+if not exist output\NHibernate ( mkdir output\NHibernate )
+SET FILES_TO_MERGE=
+SET FILES_TO_MERGE=%FILES_TO_MERGE% "src/proj/StorageAccess/bin/Release/StorageAccess.dll"
+SET FILES_TO_MERGE=%FILES_TO_MERGE% "src/proj/StorageAccess.NHibernate/bin/Release/StorageAccess.NHibernate.dll"
+bin\ilmerge-bin\ILMerge.exe /keyfile:src/StorageAccess.snk /v2 /xmldocs /out:output/NHibernate/StorageAccess.NHibernate.dll %FILES_TO_MERGE%
+copy lib\nhibernate-bin\* output\NHibernate
 
 echo Cleaning
 msbuild /nologo /verbosity:quiet src/StorageAccess.sln /p:Configuration=Release /t:Clean
