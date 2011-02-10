d7b2fb7a455a8b07591c60066673333c58b0999c
Oliver, Jonathan <joliver@localhost>
2011-02-10 13:51:41 -0700

    Removed "ParitionedStorage" it is the responsibility of a business component.

--- src/proj/StorageAccess.Core/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("StorageAccess.Core")]
-[assembly: AssemblyDescription("")]
-[assembly: Guid("a113499d-7d91-4a6d-bbb9-b530b9938939")]
